2e10da819e2fe30cd21b6c222e48f4326ff8fdf2
alex sanchez <alexjsanchez1020@gmail.com>
2023-10-12 02:12:56 -0400

    add use effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PokemonList from "./PokemonList";
 import axios from "axios";
 
 function App() {
 	const [pokemon, setPokemon] = useState(["balbasour", "charmander"]);
-	axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151").then((res) => {
-		setPokemon(res.data.results.map((p) => p.name));
-	});
+	useEffect(() => {
+		axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151").then((res) => {
+			setPokemon(res.data.results.map((p) => p));
+		});
+	}, []);
+
 	return <PokemonList pokemon={pokemon} />;
 }
 
