4b09ec02676a9694ad0dd06bad55ab6226ebd353
alex sanchez <alexjsanchez1020@gmail.com>
2023-10-12 02:01:02 -0400

    use axios to get pokemon name returned

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import PokemonList from "./PokemonList";
+import axios from "axios";
 
 function App() {
 	const [pokemon, setPokemon] = useState(["balbasour", "charmander"]);
+	axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151").then((res) => {
+		setPokemon(res.data.results.map((p) => p.name));
+	});
 	return <PokemonList pokemon={pokemon} />;
 }
 
