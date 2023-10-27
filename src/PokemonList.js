c44a096e2c930a7a1e0afa29b48dac9af50868f8
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-10-27 17:20:27 -0400

    RWD- remove margin

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -6,46 +6,46 @@ import axios from "axios";
 import PokeStats from "./PokeStats";
 
 export default function PokemonList({ pokemon }) {
-	const [number, setNumber] = useState(randomNumbers);
-	const [selectedPoke, setSelectedPoke] = useState("");
-	const [pokeDestination, setPokeDestination] = useState("");
-	const [pokeStats, setPokeStats] = useState([]);
+  const [number, setNumber] = useState(randomNumbers);
+  const [selectedPoke, setSelectedPoke] = useState("");
+  const [pokeDestination, setPokeDestination] = useState("");
+  const [pokeStats, setPokeStats] = useState([]);
 
-	useEffect(() => {
-		axios
-			.get(`https://pokeapi.co/api/v2/pokemon/${selectedPoke}`)
-			.then((res) => {
-				setPokeStats({
-					id: res.data.id,
-					name: res.data.name,
-					type: res.data.types,
-					weight: res.data.weight,
-					image: res.data.sprites,
-				});
-			});
-	}, [selectedPoke]);
-	return (
-		<>
-			<div className="App-header">
-				<PokeStats stats={pokeStats} />
-				<div className="result_box">
-					{number.map((num) => (
-						<div className="result_box_card" key={num}>
-							<p
-								className="result_box_card_name"
-								onClick={() => (
-									setPokeDestination(pokemon[num].url),
-									setSelectedPoke(pokemon[num].name)
-								)}
-								key={pokemon[num].name}
-							>
-								{pokemon[num].name.charAt(0).toUpperCase() +
-									pokemon[num].name.slice(1)}
-							</p>
-						</div>
-					))}
-				</div>
-			</div>
-		</>
-	);
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${selectedPoke}`)
+      .then((res) => {
+        setPokeStats({
+          id: res.data.id,
+          name: res.data.name,
+          type: res.data.types,
+          weight: res.data.weight,
+          image: res.data.sprites,
+        });
+      });
+  }, [selectedPoke]);
+  return (
+    <>
+      <div className="App-header">
+        <PokeStats stats={pokeStats} />
+        <div className="result_box">
+          {number.map((num) => (
+            <div className="result_box_card" key={num}>
+              <p
+                className="result_box_card_name"
+                onClick={() => (
+                  setPokeDestination(pokemon[num].url),
+                  setSelectedPoke(pokemon[num].name)
+                )}
+                key={pokemon[num].name}
+              >
+                {pokemon[num].name.charAt(0).toUpperCase() +
+                  pokemon[num].name.slice(1)}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
